10c4cbd3b6460d128b838e69d5e3313922af6262
Simamkele20 <Princetancu06@gmail.com>
2023-11-28 09:03:30 +0200

    Updates

--- script/code.js
+++ script/code.js
@@ -1,5 +1,9 @@
-let hours= +prompt("Enter your hours worked: ")
-let rate=  +prompt("Enter your rate: ")
-let salary = hours * rate
-let curformat = Intl.NumberFormat()
-console.log(`YOur salary is R${curformat.format(salary)}`)
+let arr1 = [23,2,17,20]
+let  arr2 = [56,23,87,2]
+
+let unique1 = arr1.filter((o) => arr2.indexOf(o) === -1);
+let unique2 = arr2.filter((o) => arr1.indexOf(o) === -1);
+
+const unique = unique1.concat(unique2);
+
+console.log(unique);
